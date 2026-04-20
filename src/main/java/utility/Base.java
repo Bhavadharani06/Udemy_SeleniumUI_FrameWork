3a49335bd6c0db52939a3dc0dff4c0b7f79770a4
Nisha <nishamohan225@gmail.com>
2026-04-20 09:26:45 +0530

    Resolved merge Conflict

--- src/main/java/utility/Base.java
+++ src/main/java/utility/Base.java
@@ -3,12 +3,7 @@ package utility;
 import org.openqa.selenium.WebDriver;
 
 public class Base {
-<<<<<<< HEAD
 
     public static WebDriver driver;
 
 }
-=======
-	public static WebDriver driver;
-}
->>>>>>> mylearning
--- src/main/java/utility/Pages.java
+++ src/main/java/utility/Pages.java
@@ -1,51 +1,38 @@
-package utility;
+ppackage utility;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 
 import pages.*;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.PageFactory;
-
-import pages.ArchivePage;
-import pages.LearningToolsPage;
-import pages.SignUpPage;
-import pages.MyLearningPage;
-import pages.MyListPage;
-import pages.WishlistPage;
-
 public class Pages {
-<<<<<<< HEAD
 
+    // Main pages
     public static HomePage homePage;
     public static SearchResultsPage searchResultsPage;
     public static CartPage cartPage;
-   // public static SignupPage signupPage;
+
+    // User feature pages
+    public static SignUpPage signUpPage;
+    public static MyLearningPage myLearningPage;
+    public static MyListPage myListPage;
+    public static WishlistPage wishlistPage;
+    public static ArchivePage archivePage;
+    public static LearningToolsPage learningToolsPage;
 
     public static void initPages(WebDriver driver) {
+
+        // Core pages
         homePage = PageFactory.initElements(driver, HomePage.class);
         searchResultsPage = PageFactory.initElements(driver, SearchResultsPage.class);
         cartPage = PageFactory.initElements(driver, CartPage.class);
+
+        // User pages
+        signUpPage = PageFactory.initElements(driver, SignUpPage.class);
+        myLearningPage = PageFactory.initElements(driver, MyLearningPage.class);
+        myListPage = PageFactory.initElements(driver, MyListPage.class);
+        wishlistPage = PageFactory.initElements(driver, WishlistPage.class);
+        archivePage = PageFactory.initElements(driver, ArchivePage.class);
+        learningToolsPage = PageFactory.initElements(driver, LearningToolsPage.class);
     }
 }
-=======
-	public static SignUpPage signUpPage;
-	public static MyLearningPage myLearningPage;
-	public static MyListPage myListPage;
-	public static WishlistPage wishlistPage;
-	public static ArchivePage archivePage;
-	public static LearningToolsPage learningToolsPage;
-	
-
-	public static void loadAllPages(WebDriver driver) {
-
-		signUpPage = PageFactory.initElements(driver, SignUpPage.class);
-		myLearningPage = PageFactory.initElements(driver, MyLearningPage.class);
-		myListPage = PageFactory.initElements(driver, MyListPage.class);
-		wishlistPage = PageFactory.initElements(driver, WishlistPage.class);
-		archivePage = PageFactory.initElements(driver, ArchivePage.class);
-		learningToolsPage = PageFactory.initElements(driver, LearningToolsPage.class);
-	}
-
-}
->>>>>>> mylearning
